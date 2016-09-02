97fd98d14637b4777bb47e6de92bd8a1855ffc78
He, Jiehui <he_jiehui@163.com>
2016-09-02 19:01:21 +0800

    rename id

--- com.xrosstools.common/src/com/xrosstools/common/Activator.java
+++ com.xrosstools.common/src/com/xrosstools/common/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.xross.tools.common"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.xrosstools.common"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
